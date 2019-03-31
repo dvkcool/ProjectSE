2332bc18f3f83eb487767d1a8d6aeccf860501ba
dvkcool <divyanshukumarg@gmail.com>
2019-03-31 14:01:50 +0530

    Making Server accomodate both functionalities

--- server.js
+++ server.js
@@ -5,58 +5,128 @@ var bodyParser = require('body-parser');
 require('request-debug')(request);
 var fetch =  require('fetch');
 var mysql = require('mysql');
-// var config = require("./config.json");
+var config = require("./config.json");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-// app.use(express.static(__dirname + '/public'));
-// // var pool = mysql.createPool(config);
-//
-// app.get('/pooltest', (req,res)=>{
-//   pool.query('SELECT * from bill;', function (error, results, fields) {
-//     if (error) throw error;
-//     console.log('The solution is: ', results);
-//     res.send(results);
-//   });
-// });
-//
-// // An endpoint to test from mobile application
-// app.get('/test', (req, res) =>{
-//   var done = {
-//     "sucess": 1
-//   }
-//   res.send(done);
-// });
-//
-// // An endpoint to get items of a bill based on a bill id:
-// app.get('/bill/:billId', (req,res)=>{
-//   pool.query('SELECT * FROM Bill where billId = ?',[req.params.billId], (error,results,fields)=>{
-//     if (error) throw error;
-//     //console.log('The solution is: ', rows);
-//     res.send(results);
-//   })
-// });
-//
-// // An endpoint to add a productin a particular order id:
-// app.get('/productscan/:pId', (req,res)=>{
-//   pool.query('Insert into `Bill` values(?, ?, 1)',[req.params.billId, req.params.pId], (error,results,fields)=>{
-//     if (error) throw error;
-//     //console.log('The solution is: ', rows);
-//     res.send("added succesfully");
-//   })
-// });
-//
-// app.listen(8083, function () {
-//   console.log('App listening on port 8083!');
-// });
-// ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-    //                                                                REGULAR DATABASE WORK
+
+  // CORS removal
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
+// Static HTML server
+ app.use(express.static(__dirname + '/public'));
+
+ // DB Connection
+ var pool = mysql.createPool(config);
+
+ // DB Test
+ app.get('/pooltest', (req,res)=>{
+   pool.query('SELECT * from bill;', function (error, results, fields) {
+     if (error) throw error;
+     console.log('The solution is: ', results);
+     res.send(results);
+   });
+ });
+
+  //  An endpoint to test from mobile application
+  app.get('/test', (req, res) =>{
+    var done = {
+      "sucess": 1
+    }
+    res.send(done);
+  });
+
+  //  An endpoint to get items of a bill based on a bill id:
+  app.get('/bill/:billId', (req,res)=>{
+    pool.query('SELECT * FROM Bill where billId = ?',[req.params.billId], (error,results,fields)=>{
+      if (error) throw error;
+       console.log('The solution is: ', rows);
+      res.send(results);
+    })
+  });
+
+    //DVK: An endpoint to add a productin a particular order id:
+  app.get('/productscan/:pId', (req,res)=>{
+    pool.query('Insert into `Bill` values(?, ?, 1)',[req.params.billId, req.params.pId], (error,results,fields)=>{
+      if (error) throw error;
+       console.log('The solution is: ', rows);
+      res.send("added succesfully");
+    })
+  });
+
+  //Vaibhav: query for getting product with required productid
+ app.get('/product/', (req,res)=>{
+
+   console.log("req body: ", req.body);
+   pool.query("SELECT * FROM products WHERE pid=?",[req.body.id], (err,rows,fields)=>{
+     if(!err){
+        console.log(rows);
+        res.send(rows);
+     }
+     else {
+       console.log(err);
+     }
+   })
+ });
+
+  // Vaibhav: query for deleting a product from DATABASE
+   app.get('/deletepro/', (req,res)=>{
+     var pro = req.body.name;
+     pool.query("DELETE FROM products WHERE pname=?",[pro],(err,rows,fields)=>{
+       if(!err){
+         console.log("product deleted");
+         res.send("deleted succesfully");
+       }else{
+         console.log(err);
+       }
+     })
+   });
+
+ //Vaibhav: query  for entering bill PDetails
+ app.post('/addbill/', (req,res)=>{
+     console.log("request", req.body);
+
+     var billno = req.body.billno;
+     var bdate = req.body.bdate;
+     var billamt = req.body.billamt;
+     var gst = req.body.gst;
+
+     pool.query('INSERT INTO `bills` VALUES (?,?,?,?)',[parseInt(billno), bdate, parseInt(billamt), parseFloat(gst)], (err,rows,fields)=>{
+       if(!err){
+          res.send("added succesfully");
+       }
+       else {
+         console.log(err);
+       }
+     })
+   });
+
+
+  //Vaibhav: -getting the latest bill id
+   app.get('/getlatest/', (req,res)=>{
+     pool.query('SELECT * FROM bills ORDER BY billno LIMIT 1', (err,rows,fields)=>{
+       if(!err){
+          res.send(rows);
+       }
+       else {
+         console.log(err);
+       }
+     })
+   });
+
+  // Starting the server on 8083 port
+  app.listen(8083, function () {
+    console.log('App listening on port 8083!');
+  });
+
+
+/*
+// Vaibhav:
 
 var mysqlConnection = mysql.createConnection({
     host:'localhost',
@@ -84,7 +154,7 @@ app.get('/', (req,res)=>{
        res.send("hi this is port 3000");
 });
 
-//query adding a product in DATABASE
+ query adding a product in DATABASE
 app.post('/addproduct/', (req,res)=>{
   console.log("request", req.body);
   var pid = req.body.pid;
@@ -102,7 +172,7 @@ app.post('/addproduct/', (req,res)=>{
   })
 });
 
-//query for getting product with required productid
+ query for getting product with required productid
 app.get('/product/', (req,res)=>{
 
   console.log("req body: ", req.body);
@@ -117,7 +187,7 @@ app.get('/product/', (req,res)=>{
   })
 });
 
-//query for deleting a product from DATABASE
+ query for deleting a product from DATABASE
 app.get('/deletepro/', (req,res)=>{
   var pro = req.body.name;
   mysqlConnection.query("DELETE FROM products WHERE pname=?",[pro],(err,rows,fields)=>{
@@ -130,7 +200,7 @@ app.get('/deletepro/', (req,res)=>{
   })
 })
 
-//query  for entering bill PDetails
+ query  for entering bill PDetails
 app.post('/addbill/', (req,res)=>{
   console.log("request", req.body);
 
@@ -150,7 +220,7 @@ app.post('/addbill/', (req,res)=>{
 });
 
 
-//getting the latest bill id
+ //getting the latest bill id
 app.get('/getlatest/', (req,res)=>{
   mysqlConnection.query('SELECT * FROM bills ORDER BY billno LIMIT 1', (err,rows,fields)=>{
     if(!err){
@@ -161,3 +231,4 @@ app.get('/getlatest/', (req,res)=>{
     }
   })
 })
+*/
